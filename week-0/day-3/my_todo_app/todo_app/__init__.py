54c1b6dac1c80b8d19ee1f17f2ee1cd11c727e6f
Saurav <saurav9878@gmail.com>
2019-07-16 08:32:44 +0530

    todo app v1

--- week-0/day-3/my_todo_app/todo_app/__init__.py
+++ week-0/day-3/my_todo_app/todo_app/__init__.py
@@ -3,16 +3,6 @@ import os
 from flask import Flask
 from flask import request
 
-from flask import render_template
-
-# our fake db
-todo_store = {}
-todo_store['depo'] = ['Go for run', 'Listen Rock Music']
-todo_store['shivang'] = ['Read book', 'Play Fifa', 'Drink Coffee']
-todo_store['raj'] = ['Study', 'Brush']
-todo_store['sanket'] = ['Sleep', 'Code']
-todo_store['aagam'] = ['play cricket', 'have tea']
-
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -23,50 +13,32 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    def select_todos(name):
-        global todo_store
-        return todo_store[name]
-
-    def insert_todo(name, todo):
-        global todo_store
-        current_todos = todo_store[name]
-        current_todos.append(todo)
-        todo_store[name] = current_todos
-        return
-
-    def add_todo_by_name(name, todo):
-        # call DB function
-        insert_todo(name, todo)
-        return
+    def todo_view(mytodos):
+        the_view = 'List of todos:'+ '<br/>'
+        for todo in mytodos:
+            the_view+= (todo + '<br/>')
+        
+        the_view+='--- LIST ENDS HERE ---'
+        return the_view
 
     def get_todos_by_name(name):
-        try:
-            return select_todos(name)
-        except:
-            return None
+        if name == 'depo':
+            return  ['Depo', 'Listen Rock Music']
+        elif name == 'shivang':
+            return ['Shivang', 'Listen Rock Music']
+        elif name == 'raj':
+            return ['Raj', 'Listen Rock Music']
+        else:
+            return []
 
-
-    # http://127.0.0.1:5000/todos?name=duster
     @app.route('/todos')
     def todos():
         name = request.args.get('name')
-        print('---------')
         print(name)
-        print('---------')
-
         person_todo_list = get_todos_by_name(name)
-        if person_todo_list == None:
-            return render_template('404.html'), 404
-        else:
-            return render_template('todo_view.html',todos=person_todo_list)
+        return todo_view(person_todo_list)
 
-
-    @app.route('/add_todos')
-    def add_todos():
-        name = request.args.get('name')
-        todo = request.args.get('todo')
-        add_todo_by_name(name, todo)
-        return 'Added Successfully'
-
+    # a simple page that list my todos
+    
     return app
 
